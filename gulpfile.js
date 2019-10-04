9cbab6b43d5e68f77c6948ab4d9910f179dd8deb
Eddie Anguiano <eddiesanguiano@gmail.com>
2019-10-03 22:28:13 -0700

    added various gulp plugins and configured them to file system

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,99 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const changed = require('gulp-changed');
+const uglify = require('gulp-uglify');
+const lineec = require('gulp-line-ending-corrector');
 
-// compile scss into css
-function style() {
-  // 1. find my scss file
-  // 2. pass that file through sass compiler
-  // 3. where do i save the compiled css?
-  // 4. strem changes to all browsers
+// location of main scss file
+const sassSrc = './src/styles/main.scss';
+// location of css folder to send compiled scss
+const compiledSass = 'src/css';
+
+function css() {
   return gulp
-    .src('./src/styles/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./build/styles/'))
-    .pipe(browserSync.stream());
+    .src(sassSrc)
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(
+      sass({
+        outputStyle: 'expanded'
+      }).on('error', sass.logError)
+    )
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(sourcemaps.write())
+    .pipe(lineec())
+    .pipe(gulp.dest(compiledSass));
 }
+
+// css files to be concatenated and minified (vendor files first)
+const cssSrc = ['src/css/main.css'];
+// location on final minified css file
+const buildCSS = 'build/styles';
+// name of minified css file
+const minCSS = 'main.min.css';
+
+function concatCSS() {
+  return gulp
+    .src(cssSrc)
+    .pipe(sourcemaps.init({ loadMaps: true, largeFile: true }))
+    .pipe(concat(minCSS))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./maps/'))
+    .pipe(lineec())
+    .pipe(gulp.dest(buildCSS));
+}
+
+// concats js files in specific order (vendors first)
+const jsSRC = ['src/scripts/main.js'];
+// name of minified js file
+const minJS = 'main.min.js';
+// destination of minified js file
+const buildJS = 'build/scripts';
+
+function javascript() {
+  return gulp
+    .src(jsSRC)
+    .pipe(concat(minJS))
+    .pipe(uglify())
+    .pipe(lineec())
+    .pipe(gulp.dest(buildJS));
+}
+
+// images source folders/files
+const imgSrc = 'src/images/**/*';
+// images build file
+const imgBuild = 'build/images/';
+
+function imgmin() {
+  return gulp
+    .src(imgSrc)
+    .pipe(changed(imgBuild))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 })
+      ])
+    )
+    .pipe(gulp.dest(imgBuild));
+}
+
+// js files to watch
+const jsWatch = './src/scripts/**/*.js';
+// scss files to watch
+const scssWatch = './src/styles/**/*.scss';
+// image files to watch
+const imgWatch = 'src/images/**/*';
+
+// browser reload on changes to these files
+const jsReload = `build/scripts/main.min.js`;
+const cssReload = `build/styles/main.min.css`;
+const htmlReload = './build/*.html';
 
 function watch() {
   browserSync.init({
@@ -21,10 +101,17 @@ function watch() {
       baseDir: './build/'
     }
   });
-  gulp.watch('./src/styles/**/*.scss', style);
-  gulp.watch('./build/*.html').on('change', browserSync.reload);
-  gulp.watch('./src/scripts/**/*.js').on('change', browserSync.reload);
+  gulp.watch(scssWatch, gulp.series([css, concatCSS]));
+  gulp.watch(jsWatch, javascript);
+  gulp.watch(imgWatch, imgmin);
+  gulp.watch([jsReload, cssReload, htmlReload]).on('change', browserSync.reload);
 }
 
-exports.style = style;
+exports.css = css;
+exports.concatCSS = concatCSS;
+exports.javascript = javascript;
 exports.watch = watch;
+exports.imgmin = imgmin;
+
+const build = gulp.parallel(watch);
+gulp.task('default', build);
